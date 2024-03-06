486287f0cbce9e7683588911d1afdb77af43e367
Tuong-Luan Bach <bachxtuongluan@gmail.com>
2024-03-06 15:44:37 +0000

    Updated server, Client and Clienthandler file Added the ability to ask for user details Added the ability to privatly message people Added new file coodinator.java

---
+++ Coordinator.java
@@ -0,0 +1,11 @@
+public class Coordinator {
+    private static ClientHandler currentCoordinator;
+
+    public static synchronized void setCoordinator(ClientHandler clientHandler) {
+        currentCoordinator = clientHandler;
+    }
+
+    public static synchronized ClientHandler getCurrentCoordinator() {
+        return currentCoordinator;
+    }
+}
